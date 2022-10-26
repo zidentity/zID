9ca0360d0fb059cf713d704413781c9e1967ad90
KaiStryker <kareemj.williams@gmail.com>
2022-10-26 19:41:51 +0100

    zkWallet.sol + .ts scripts

---
+++ contracts/zkWallet.sol
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.9;
+
+/* solhint-disable reason-string */
+
+import "./lib/account-abstraction/contracts/samples/SimpleWallet.sol";
+import "./lib/contracts/contracts/verifiers/ZKPVerifier.sol";
+
+contract zkWallet is ZKPVerifier, SimpleWallet {
+    using UserOperationLib for UserOperation;
+
+    uint64 public constant OWNER_REQUEST_ID = 1;
+
+    constructor(
+        IEntryPoint anEntryPoint, 
+        address anOwner,
+        uint64 requestId,
+        ICircuitValidator validator,
+        ICircuitValidator.CircuitQuery memory query
+    ) SimpleWallet(anEntryPoint, anOwner) {
+        bool zkRequestSet = _setZKPRequest(requestId, validator, query);
+        require(zkRequestSet, "zkRequest not set");
+    }
+
+    function _validateSignature(UserOperation calldata userOp, bytes32 requestId, address) internal virtual override {
+        (   
+            uint64 requestId,
+            uint256[] memory inputs,
+            uint256[2] memory a,
+            uint256[2][2] memory b,
+            uint256[2] memory c
+        ) = abi.decode(userOp.signature, uint64, uint256[], uint256[2], uint256[2][2], uint256[2]);
+        
+        bool validProof = submitZKPResponse(
+            requestId,
+            inputs,
+            a,
+            b,
+            c
+        );
+        
+        require(validProof, "Proof not valid");
+    }
+
+    function _setZKPRequest(
+        uint64 requestId,
+        ICircuitValidator validator,
+        ICircuitValidator.CircuitQuery memory query
+    ) internal returns (bool) {
+        if (requestValidators[requestId] == ICircuitValidator(address(0x00))) {
+            supportedRequests.push(requestId);
+        }
+        requestQueries[requestId].value = query.value;
+        requestQueries[requestId].operator = query.operator;
+        requestQueries[requestId].circuitId = query.circuitId;
+        requestQueries[requestId].slotIndex = query.slotIndex;
+        requestQueries[requestId].schema = query.schema;
+
+        requestQueries[requestId].circuitId = query.circuitId;
+
+        requestValidators[requestId] = validator;
+        return true;
+    }
+}
---
+++ scripts/deployZKWallet.ts
@@ -0,0 +1,59 @@
+import { ethers } from "hardhat";
+
+async function main() {
+    const circuitId = "credentialAtomicQuerySig";
+    const validatorAddress = "0xb1e86C4c687B85520eF4fd2a0d14e81970a15aFB";
+
+    // Grab the schema hash from Polygon ID Platform
+    const schemaHash = "schemaHash"
+
+    const schemaEnd = fromLittleEndian(hexToBytes(schemaHash))
+
+    const ownerQuery = {
+        schema: ethers.BigNumber.from(schemaEnd),
+        slotIndex: 2,
+        operator: 2,
+        value: [20020101, ...new Array(63).fill(0).map(i => 0)],
+        circuitId,
+    };
+
+    // add the address of the contract just deployed
+    const contractName ="zkWallet"
+    const entryPoint = "ERC20ZKPVerifier";
+    const requestId = "1";
+    const ZkWallet = await ethers.getContractFactory(contractName);
+    const zkWallet = await ZkWallet.deploy(
+        entryPoint,
+        ethers.constants.AddressZero,
+        requestId,
+        validatorAddress,
+        ownerQuery
+    )
+
+    console.log(`txHash: ${zkWallet.deployTransaction.hash}`)
+    await zkWallet.deployed()
+}
+
+function hexToBytes(hex: any) {
+    for (var bytes = [], c = 0; c < hex.length; c += 2)
+        bytes.push(parseInt(hex.substr(c, 2), 16));
+    return bytes;
+}
+
+function fromLittleEndian(bytes: any) {
+    const n256 = BigInt(256);
+    let result = BigInt(0);
+    let base = BigInt(1);
+    bytes.forEach((byte: any) => {
+      result += base * BigInt(byte);
+      base = base * n256;
+    });
+    return result;
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
